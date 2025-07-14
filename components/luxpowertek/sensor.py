bfa4d8ea4a2ab1cb12aefe1861bd9d5655fb82d1
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-14 06:22:14 +0000

    commit

--- components/luxpowertek/sensor.py
+++ components/luxpowertek/sensor.py
@@ -17,12 +17,22 @@ LuxPowertekComponent = luxpowertek_ns.class_("LuxPowertekComponent", cg.Componen
 
 LuxPowertekSensor = luxpowertek_ns.class_("LuxPowertekSensor", sensor.Sensor)
 
-CONF_TYPE = "type"
-TYPES = ["soc", "vbat", "p_discharge"]
+SENSOR_TYPES = {
+    # System sensors
+    "soc": sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, device_class=DEVICE_CLASS_BATTERY, state_class=STATE_CLASS_MEASUREMENT, accuracy_decimals=0),
+    "vbat": sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, device_class=DEVICE_CLASS_VOLTAGE, state_class=STATE_CLASS_MEASUREMENT, accuracy_decimals=1, icon="mdi:battery"),
+    "p_discharge": sensor.sensor_schema(unit_of_measurement=UNIT_WATT, device_class=DEVICE_CLASS_POWER, state_class=STATE_CLASS_MEASUREMENT, accuracy_decimals=0, icon="mdi:battery-minus"),    
+}
 
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
-    cv.Required(CONF_TYPE): cv.one_of(*TYPES),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.All(
+    LUXPOWERTEK_COMPONENT_SCHEMA.extend(
+        {
+            **{cv.Optional(key): schema for key, schema in SENSOR_TYPES.items()},
+            cv.Optional(CONF_UPDATE_INTERVAL, default="20s"): cv.update_interval,
+        }
+    ),    
+    cv.has_at_least_one_key(*SENSOR_TYPES.keys()),
+)
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_ID])
