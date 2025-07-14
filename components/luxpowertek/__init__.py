2988da65944b79b7082beec7a2659e82c6750f2c
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-14 06:00:58 +0000

    commit

--- components/luxpowertek/__init__.py
+++ components/luxpowertek/__init__.py
@@ -1,31 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import (
-    CONF_ID,
-    CONF_HOST,
-    CONF_PORT,
-    CONF_UPDATE_INTERVAL,
-)
-
-CONF_DONGLE_SERIAL = "dongle_serial"
-CONF_INVERTER_SN = "inverter_serial_number"
+from esphome.const import CONF_ID
 
 luxpowertek_ns = cg.esphome_ns.namespace("luxpowertek")
 LuxPowertekComponent = luxpowertek_ns.class_("LuxPowertekComponent", cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LuxPowertekComponent),
-    cv.Required(CONF_HOST): cv.ipv4,
-    cv.Required(CONF_PORT): cv.port,
-    cv.Required(CONF_DONGLE_SERIAL): cv.string_strict,
-    cv.Required(CONF_INVERTER_SN): cv.string_strict,
-    cv.Optional(CONF_UPDATE_INTERVAL, default="20s"): cv.update_interval,
+    cv.Required("host"): cv.ipv4,
+    cv.Required("port"): cv.port,
+    cv.Required("dongle_serial"): cv.string_strict,
+    cv.Required("inverter_serial_number"): cv.string_strict,
+    cv.Optional("update_interval", default="20s"): cv.update_interval,
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add(var.set_host(config[CONF_HOST]))
-    cg.add(var.set_port(config[CONF_PORT]))
-    cg.add(var.set_dongle_serial(config[CONF_DONGLE_SERIAL]))
-    cg.add(var.set_inverter_serial(config[CONF_INVERTER_SN]))
+    cg.add(var.set_host(config["host"]))
+    cg.add(var.set_port(config["port"]))
+    cg.add(var.set_dongle_serial(config["dongle_serial"]))
+    cg.add(var.set_inverter_serial(config["inverter_serial_number"]))
