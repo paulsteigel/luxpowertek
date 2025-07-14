599aefcfa7df3853a057eb1535f733fbaba494cd
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-14 06:48:07 +0000

    commit

--- components/luxpowertek/__init__.py
+++ components/luxpowertek/__init__.py
@@ -1,23 +1,13 @@
-# components/luxpowertek/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-#from esphome.core import CORE, coroutine_with_priority
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["wifi"]
-AUTO_LOAD = ["sensor", "text_sensor"] # Make sure text_sensor is also loaded
-MULTI_CONF = True
 
 luxpowertek_ns = cg.esphome_ns.namespace("luxpowertek")
-LuxpowertekComponent = luxpowertek_ns.class_("LuxpowertekComponent", cg.PollingComponent)
+LuxpowertekComponent = luxpowertek_ns.class_("LuxPowertekComponent", cg.PollingComponent)
 
 CONF_LUXPOWERTEK_ID = "luxpowertek_id"
-
-LUXPOWERTEK_COMPONENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_LUXPOWERTEK_ID): cv.use_id(LuxpowertekComponent),
-    }
-)
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -35,20 +25,7 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
     cg.add(var.set_host(config["host"]))
     cg.add(var.set_port(config["port"]))
-
-    # --- Corrected Code Generation ---
-    # We pass the string directly from the config.
-    # The C++ side will handle validation and conversion.
-    dongle_serial = config["dongle_serial"]
-    if len(dongle_serial) != 10:
-        raise cv.Invalid("dongle_serial must be 10 characters long")
-    cg.add(var.set_dongle_serial(dongle_serial))
-
-    inverter_serial = config["inverter_serial_number"]
-    if len(inverter_serial) != 10:
-        raise cv.Invalid("inverter_serial_number must be 10 characters long")
-    # This call must match the function name in the .h file exactly.
-    cg.add(var.set_inverter_serial_number(inverter_serial))
+    cg.add(var.set_dongle_serial(config["dongle_serial"]))
+    cg.add(var.set_inverter_serial_number(config["inverter_serial_number"]))
--- components/luxpowertek/sensor.py
+++ components/luxpowertek/sensor.py
@@ -2,42 +2,51 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
-    CONF_NAME,
-    DEVICE_CLASS_BATTERY,
-    DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_POWER,
     UNIT_PERCENT,
     UNIT_VOLT,
     UNIT_WATT,
+    DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_VOLTAGE,
+    DEVICE_CLASS_POWER,
     STATE_CLASS_MEASUREMENT,
-    CONF_UPDATE_INTERVAL,
+    CONF_ID,
 )
-from . import LUXPOWERTEK_COMPONENT_SCHEMA, CONF_LUXPOWERTEK_ID
 
-luxpowertek_ns = cg.esphome_ns.namespace("luxpowertek")
+from . import luxpowertek_ns, CONF_LUXPOWERTEK_ID, LUXPOWERTEK_COMPONENT_SCHEMA
+
 LuxPowertekComponent = luxpowertek_ns.class_("LuxPowertekComponent", cg.Component)
 
-LuxPowertekSensor = luxpowertek_ns.class_("LuxPowertekSensor", sensor.Sensor)
-
 SENSOR_TYPES = {
-    # System sensors
-    "lux_soc": sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, device_class=DEVICE_CLASS_BATTERY, state_class=STATE_CLASS_MEASUREMENT, accuracy_decimals=0),
-    "lux_vbat": sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, device_class=DEVICE_CLASS_VOLTAGE, state_class=STATE_CLASS_MEASUREMENT, accuracy_decimals=1, icon="mdi:battery"),
-    "lux_p_discharge": sensor.sensor_schema(unit_of_measurement=UNIT_WATT, device_class=DEVICE_CLASS_POWER, state_class=STATE_CLASS_MEASUREMENT, accuracy_decimals=0, icon="mdi:battery-minus"),    
+    "lux_soc": sensor.sensor_schema(
+        unit_of_measurement=UNIT_PERCENT,
+        device_class=DEVICE_CLASS_BATTERY,
+        state_class=STATE_CLASS_MEASUREMENT,
+        accuracy_decimals=0,
+    ),
+    "lux_vbat": sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+        accuracy_decimals=1,
+    ),
+    "lux_p_discharge": sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        device_class=DEVICE_CLASS_POWER,
+        state_class=STATE_CLASS_MEASUREMENT,
+        accuracy_decimals=0,
+    ),
 }
 
-CONFIG_SCHEMA = cv.All(
-    LUXPOWERTEK_COMPONENT_SCHEMA.extend(
-        {
-            **{cv.Optional(key): schema for key, schema in SENSOR_TYPES.items()},
-            cv.Optional(CONF_UPDATE_INTERVAL, default="20s"): cv.update_interval,
-        }
-    ),    
-    cv.has_at_least_one_key(*SENSOR_TYPES.keys()),
-)
+CONFIG_SCHEMA = LUXPOWERTEK_COMPONENT_SCHEMA.extend({
+    cv.GenerateID(CONF_ID): cv.declare_id(LuxPowertekComponent),
+    **{cv.Optional(key): schema for key, schema in SENSOR_TYPES.items()}
+})
+
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_LUXPOWERTEK_ID])
-    sens = await sensor.new_sensor(config)
-    cg.add(hub.register_sensor(config[CONF_TYPE], sens))
+
+    for key in SENSOR_TYPES:
+        if key in config:
+            sens = await sensor.new_sensor(config[key])
+            cg.add(hub.register_sensor(key, sens))
